c954715ee33bd4768526d1a205fc850a8b816be0
Jakub Sobczyński <115147227+Maszyna1337@users.noreply.github.com>
2023-03-29 16:55:37 +0200

    Update and rename probny.py to probny2.py

--- probny2.py
+++ probny2.py
@@ -1,15 +1,15 @@
 import math
 #zadanie1
-#tekst = open("tekst1.txt", "r+", encoding="utf-8")
-#odczyt = tekst.read()
-#znaki = []
-#for i in range(71, 111):
-#    znaki.append(odczyt[i])
-#a = znaki.count("a")
-#if a == 0:
-#   print("Brak liter")
-#else:
-#    print(a)
+tekst = open("tekst1.txt", "r+", encoding="utf-8")
+odczyt = tekst.read()
+znaki = []
+for i in range(71, 111):
+    znaki.append(odczyt[i])
+a = znaki.count("a")
+if a == 0:
+   print("Brak liter")
+else:
+    print(a)
 #Zadanie2
 lista = [0.3, 2, 1.4, 3]
 listafl = [lista[i] for i in range(0, len(lista)) if type(lista[i]) == float]
@@ -34,14 +34,14 @@ wynik = a+pierwiastek
 print(round(wynik, 2))
 
 #Zadanie5
-#plik = open("plik.txt", "w+")
+plik = open("plik.txt", "w+")
 n = int(input("Podaj n"))
 wynik = 0
 try:
     for i in range(1, n+1):
         wynik+=i
-    #plik.write(wynik)
-    #plik.close()
+    plik.write(wynik)
+    plik.close()
 except ValueError:
     if n<0:
         print("Liczba musi byc dodatnia")
